af9be51afb4338b2f0f43d0762e9e8ba074c08d0
Justice Hanson <just.hanson1@gmail.com>
2024-08-31 13:50:00 +0000

    modify

--- pages/2_🔮_Predict.py
+++ pages/2_🔮_Predict.py
@@ -15,13 +15,10 @@ st.set_page_config(
 )
 
 # --------- Add custom CSS to adjust the width of the sidebar
-st.markdown( """ <style> 
-            section[data-testid="stSidebar"]
-            { width: 200px !important;
-            }
-            </style> """,
-            unsafe_allow_html=True,
-)
+st.markdown(""" 
+    <style> 
+        section[data-testid="stSidebar"] { width: 200px !important; }
+    </style> """, unsafe_allow_html=True)
 
 st.title("Predict Customer Churn!")
 
@@ -58,11 +55,9 @@ def load_encoder():
 
 # --------- Create a function for model selection
 def select_model():
-    # ------- Option for first model
     if model_option == 'Gradient Boosting':
         model = gb_pipeline()
-    # ------- Option for second model
-    if model_option == 'Support Vector':
+    else:  # Ensure 'Support Vector' is handled correctly
         model = sv_pipeline()
     encoder = load_encoder()
     return model, encoder
@@ -75,21 +70,17 @@ class TotalCharges_cleaner(BaseEstimator, TransformerMixin):
     def transform(self, X):
         # Replace empty string with NA
         X['TotalCharges'].replace(' ', np.nan, inplace=True)
-
-        # Convert the values in the Totalcharges column to a float
-        X['TotalCharges'] = X['TotalCharges'].transform(lambda x: float(x))
+        # Convert the values in the TotalCharges column to a float
+        X['TotalCharges'] = X['TotalCharges'].astype(float)
         return X
         
     # Serialization methods
     def __getstate__(self):
-        # Return state to be serialized
         return {}
 
     def __setstate__(self, state):
-        # Restore state from serialized data
         pass
         
-    # Since this transformer doesn't remove or alter features, return the input features
     def get_feature_names_out(self, input_features=None):
         return input_features
 
@@ -99,17 +90,14 @@ class columnDropper(BaseEstimator, TransformerMixin):
         return self
         
     def transform(self, X):
-        # Drop the specified column
         return X.drop('customerID', axis=1)
         
     def get_feature_names_out(self, input_features=None):
-        # If input_features is None or not provided, return None
         if input_features is None:
             return None
-        # Return feature names after dropping the specified column
         return [feature for feature in input_features if feature != 'customerID']
 
-# ---- Initialize prediction in session state
+# Initialize prediction in session state
 if 'prediction' not in st.session_state:
     st.session_state['prediction'] = None
 if 'prediction_proba' not in st.session_state:
@@ -117,7 +105,6 @@ if 'prediction_proba' not in st.session_state:
 
 # ------- Create a function to make prediction
 def make_prediction(model, encoder):
-
     customerID = st.session_state['customer_id']
     gender = st.session_state['gender']
     SeniorCitizen = st.session_state['senior_citizen']
@@ -151,25 +138,27 @@ def make_prediction(model, encoder):
         
     data = pd.DataFrame(values, columns=columns)
 
-    # -------- Get the value for prediction
+    # Get the value for prediction
     prediction = model.predict(data)
     prediction = encoder.inverse_transform(prediction)
     st.session_state['prediction'] = prediction
 
-    # -------- Get the value for prediction probability
+    # Get the value for prediction probability
     prediction_proba = model.predict_proba(data)
     st.session_state['prediction_proba'] = prediction_proba
 
     data['Churn'] = prediction
     data['Model'] = model_option
 
+    if not os.path.exists('./data'):
+        os.makedirs('./data')
+        
     data.to_csv('./data/history.csv', mode='a', header=not os.path.exists('./data/history.csv'), index=False)
 
     return prediction, prediction_proba
 
 # ------- Prediction page creation
 def input_features():
-
     with st.form('features'):
         model_pipeline, encoder = select_model()
         col1, col2 = st.columns(2)
@@ -178,10 +167,10 @@ def input_features():
         with col1:
             st.subheader('Demographics')
             st.text_input('Customer ID', value="", placeholder='eg. 1234-ABCDE', key='customer_id')
-            st.radio('Gender', options=['Male', 'Female'],horizontal=True, key='gender',)
-            st.radio('Partners', options=['Yes', 'No'],horizontal=True, key='partners')
-            st.radio('Dependents', options=['Yes', 'No'],horizontal=True, key='dependents')
-            st.radio("Senior Citizen ('Yes-1, No-0')", options=[1, 0],horizontal=True, key='senior_citizen')
+            st.radio('Gender', options=['Male', 'Female'], horizontal=True, key='gender')
+            st.radio('Partners', options=['Yes', 'No'], horizontal=True, key='partners')
+            st.radio('Dependents', options=['Yes', 'No'], horizontal=True, key='dependents')
+            st.radio("Senior Citizen ('Yes-1, No-0')", options=[1, 0], horizontal=True, key='senior_citizen')
             
         # ------ Collect customer account information
         with col1:
@@ -189,8 +178,8 @@ def input_features():
             st.number_input('Tenure', min_value=0, max_value=70, key='tenure')
             st.selectbox('Contract', options=['Month-to-month', 'One year', 'Two year'], key='contract')
             st.selectbox('Payment Method',
-                                        options=['Electronic check', 'Mailed check', 'Bank transfer (automatic)', 'Credit card (automatic)'],
-                                        key='payment_method')
+                         options=['Electronic check', 'Mailed check', 'Bank transfer (automatic)', 'Credit card (automatic)'],
+                         key='payment_method')
             st.radio('Paperless Billing', ['Yes', 'No'], horizontal=True, key='paperless_billing')
             st.number_input('Monthly Charges', placeholder='Enter amount...', key='monthly_charges')
             st.number_input('Total Charges', placeholder='Enter amount...', key='total_charges')
@@ -199,43 +188,44 @@ def input_features():
         with col2:
             st.subheader('Subscriptions')
             st.radio('Phone Service', ['Yes', 'No'], horizontal=True, key='phone_service')
-            st.selectbox('Multiple Lines', ['Yes', 'No', 'No internet servie'], key='multiple_lines')
-            st.selectbox('Internet Service', ['DSL','Fiber optic', 'No'], key='internet_service')
-            st.selectbox('Online Security', ['Yes', 'No', 'No internet servie'], key='online_security')
-            st.selectbox('Online Backup', ['Yes', 'No', 'No internet servie'], key='online_backup')
-            st.selectbox('Device Protection', ['Yes', 'No', 'No internet servie'], key='device_protection')
-            st.selectbox('Tech Support', ['Yes', 'No', 'No internet servie'], key='tech_support')
-            st.selectbox('Streaming TV', ['Yes', 'No', 'No internet servie'], key='streaming_tv')
-            st.selectbox('Streaming Movies', ['Yes', 'No', 'No internet servie'], key='streaming_movies')
-            
-        st.form_submit_button('Predict', on_click=make_prediction, kwargs=dict(model=model_pipeline, encoder=encoder))
+            st.selectbox('Multiple Lines', ['Yes', 'No', 'No internet service'], key='multiple_lines')
+            st.selectbox('Internet Service', ['DSL', 'Fiber optic', 'No'], key='internet_service')
+            st.selectbox('Online Security', ['Yes', 'No', 'No internet service'], key='online_security')
+            st.selectbox('Online Backup', ['Yes', 'No', 'No internet service'], key='online_backup')
+            st.selectbox('Device Protection', ['Yes', 'No', 'No internet service'], key='device_protection')
+            st.selectbox('Tech Support', ['Yes', 'No', 'No internet service'], key='tech_support')
+            st.selectbox('Streaming TV', ['Yes', 'No', 'No internet service'], key='streaming_tv')
+            st.selectbox('Streaming Movies', ['Yes', 'No', 'No internet service'], key='streaming_movies')
 
+        # Add the submit button
+        submit_button = st.form_submit_button('Predict', on_click=make_prediction, kwargs=dict(model=model_pipeline, encoder=encoder))
     return True
 
-if __name__=='__main__':
+if __name__ == '__main__':
     input_features()
+    
     prediction = st.session_state['prediction']
     probability = st.session_state['prediction_proba']    
 
-    if st.session_state['prediction'] == None:
+    if prediction is None:
         cols = st.columns([3, 4, 3])
         with cols[1]:
             st.markdown('#### Predictions will show here ⤵️')
-        cols = st.columns([.25,.5,.25])
+        cols = st.columns([.25, .5, .25])
         with cols[1]:
-            st.markdown('##### No predictions made yet. Make prediction')
+            st.markdown('##### No predictions made yet. Make a prediction.')
     else:
         if prediction == "Yes":
-            cols = st.columns([.1,.8,.1])
+            cols = st.columns([.1, .8, .1])
             with cols[1]:
-                st.markdown(f'### The customer will churn with a {round(probability[0][1],2)} probability.')
-            cols = st.columns([.3,.4,.3])
+                st.markdown(f'### The customer will churn with a {round(probability[0][1], 2)} probability.')
+            cols = st.columns([.3, .4, .3])
             with cols[1]:
-                st.success('Churn status predicted successfullly🎉')
+                st.success('Churn status predicted successfully🎉')
         else:
-            cols = st.columns([.1,.8,.1])
+            cols = st.columns([.1, .8, .1])
             with cols[1]:
-                st.markdown(f'### The customer will not churn with a {round(probability[0][0],2)} probability.')
-            cols = st.columns([.3,.4,.3])
+                st.markdown(f'### The customer will not churn with a {round(probability[0][0], 2)} probability.')
+            cols = st.columns([.3, .4, .3])
             with cols[1]:
-                st.success('Churn status predicted successfullly🎉')
+                st.success('Churn status predicted successfully🎉')
